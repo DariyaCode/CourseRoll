c6ed5f0db7e4ddeb133ee31c8eb78bf39c1a6b5d
DariyaCode <dariya.gecher@gmail.com>
2023-02-15 22:31:53 +0600

    To shorten the code and implement the DRY coding rules, I created a separate file for styles

---
+++ ScrollDer/Style.swift
@@ -0,0 +1,15 @@
+//
+//  Style.swift
+//  ScrollDer
+//
+//  Created by Dariya Gecher on 15.02.2023.
+//
+
+import SwiftUI
+
+struct Style: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
