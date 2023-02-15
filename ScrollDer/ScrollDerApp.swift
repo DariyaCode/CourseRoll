cccef7cd7b6999b9c0fd8ab287d7545ffa50e355
DariyaCode <dariya.gecher@gmail.com>
2023-02-15 17:33:23 +0600

    Initial Commit

---
+++ ScrollDer/ScrollDerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollDerApp.swift
+//  ScrollDer
+//
+//  Created by Dariya Gecher on 15.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrollDerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
