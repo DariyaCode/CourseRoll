267a94866e18360fe7c5656db7c42a4a6bf3a662
DariyaCode <dariya.gecher@gmail.com>
2023-02-17 00:17:10 +0600

    I'm reorganized the structure of code in AccountView file. There now the body structure is so comfortable to read it. Minimum  lines of  code here. Now i just use link of variable, that contains the all of un-readeble code.

--- ScrollDer/AccountView.swift
+++ ScrollDer/AccountView.swift
@@ -9,88 +9,100 @@ import SwiftUI
 
 struct AccountView: View {
     @State var isDeleted = false
+    @State var isPinned = false
     
     var body: some View {
         NavigationView {
             List{
-                VStack(spacing: 8) {
-                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
-                        .symbolVariant(.circle.fill)
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(Image(systemName: "hexagon")
-                            .symbolVariant(.fill)
-                            .foregroundStyle(.blue)
-                            .font(.system(size: 200))
-                            .offset(x: -50, y: -100)
-                    )
-                    Text("Dariya Gecher")
-                        .font(.title.weight(.semibold))
-                    HStack {
-                        Image(systemName: "location")
-                            .foregroundColor(.secondary)
-                        Text("Kazakhstan")
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
-                Section {
-                    NavigationLink(destination: ContentView()) {
-                        Label("Settings", systemImage: "gear")
-                    }
-                    NavigationLink { ContentView() } label: {
-                        Label("Pricing", systemImage: "creditcard")
-                    }
-                    NavigationLink { ContentView() } label: {
-                        Label("Help", systemImage: "questionmark")
-                    }
-                }
-                .foregroundColor(.primary)
-                .listRowSeparator(.hidden)
+                profile
                 
+                menu
                 
-                Section {
-                    if !isDeleted {
-                        Link(destination: URL(string: "https://github.com/DariyaCode/CourseRoll")!) {
-                            HStack {
-                                Label("Website", systemImage: "house")
-                                Spacer()
-                                Image(systemName: "link")
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                        .swipeActions(edge: .leading) {
-                            Button(action: {isDeleted = true})
-                            {
-                                Label("Delete", systemImage: "trash")
-                            }
-                            .tint(.red)
-                            Button {} label: {
-                                Label("Pin", systemImage: "pin")
-                            }
-                            .tint(.yellow)
-                    }
-                    }
-                    Link(destination: URL(string: "https://youtube.com")!) {
-                        HStack {
-                            Label("Youtube", systemImage: "tv")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                }
-                .foregroundColor(.primary)
-                .listRowSeparator(.hidden)
-
+                links
                 
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
         }
+    }
+    var profile: some View{
+        VStack(spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(Image(systemName: "hexagon")
+                    .symbolVariant(.fill)
+                    .foregroundStyle(.blue)
+                    .font(.system(size: 200))
+                    .offset(x: -50, y: -100)
+                )
+            Text("Dariya Gecher")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .foregroundColor(.secondary)
+                Text("Kazakhstan")
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    var menu: some View{
+        Section {
+            NavigationLink(destination: ContentView()) {
+                Label("Settings", systemImage: "gear")
+            }
+            NavigationLink { ContentView() } label: {
+                Label("Pricing", systemImage: "creditcard")
+            }
+            NavigationLink { ContentView() } label: {
+                Label("Help", systemImage: "questionmark")
+            }
+        }
+        .foregroundColor(.primary)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View{
+        Section {
+            if !isDeleted {
+                Link(destination: URL(string: "https://github.com/DariyaCode/CourseRoll")!) {
+                    HStack {
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .swipeActions(edge: .leading) {
+                    Button(action: {isDeleted = true})
+                    {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red)
+                    Button {} label: {
+                        Label("Pin", systemImage: "pin")
+                    }
+                    .tint(.yellow)
+                }
+            }
+            Link(destination: URL(string: "https://youtube.com")!) {
+                HStack {
+                    Label("Youtube", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+        .foregroundColor(.primary)
+        .listRowSeparator(.hidden)
+        
     }
 }
 
