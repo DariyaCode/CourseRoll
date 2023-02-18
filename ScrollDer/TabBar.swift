387142f12495d7e464b172cfe61ee2ddf934e0bd
DariyaCode <dariya.gecher@gmail.com>
2023-02-19 02:51:05 +0600

    Try to add the tabbar to app. It's a test commit for it

---
+++ ScrollDer/TabBar.swift
@@ -0,0 +1,46 @@
+//
+//  TabBar.swift
+//  ScrollDer
+//
+//  Created by Dariya Gecher on 19.02.2023.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            ContentView()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .offset(y: 300)
+            HStack {
+                Spacer()
+                
+                    VStack(spacing: 0) {
+                        Image(systemName: "house")
+                            .symbolVariant(.fill)
+                            .font(.body.bold())
+                            .frame(width: 44, height: 29)
+                        Text("Learn Now!")
+                            .font(.caption2)
+                    
+                    Spacer()
+                }
+            }
+            .padding(.top, 14)
+            .frame(height: 88, alignment: .top)
+            .background(.ultraThinMaterial, in:
+                            RoundedRectangle(cornerRadius: 34, style: .continuous)
+            )
+            .strokeStyle(cornerRadius: 34)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+            .ignoresSafeArea()
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
