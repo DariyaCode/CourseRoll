94bedd677d5da804323e148d340510c73ecd43a5
DariyaCode <dariya.gecher@gmail.com>
2023-02-19 14:21:21 +0600

    Now i led TabBar to working condition. It remains only to work on the style!

--- ScrollDer/Tab.swift
+++ ScrollDer/Tab.swift
@@ -11,11 +11,19 @@ struct TableItem: Identifiable {
     var id = UUID()
     var text: String
     var icon: String
+    var tab: Tab
 }
 
 var tabItems = [
-    TableItem(text: "Learn now!", icon: "house"),
-    TableItem(text: "Explore", icon: "magnifyingglass"),
-    TableItem(text: "Notifications", icon: "bell"),
-    TableItem(text: "Library", icon: "rectangle.stack"),
+    TableItem(text: "Learn now!", icon: "house", tab: .home),
+    TableItem(text: "Explore", icon: "magnifyingglass", tab: .explore),
+    TableItem(text: "Notifications", icon: "bell", tab: .notifications),
+    TableItem(text: "Library", icon: "rectangle.stack", tab: .library),
 ]
+
+enum Tab: String{
+    case home
+    case explore
+    case notifications
+    case library
+}
