3e98b5f20f8556cc97022a5194794f84239356f7
DariyaCode <dariya.gecher@gmail.com>
2023-02-19 15:35:59 +0600

    Created an adequate architecture for the project.  Arranged each file in the directories where they should be located.  I stick to clean architecture. In addition, I slightly edited the paths in the files.  Created a new HomeView file, which is considered the title screen.  Moved the code from one file to another, where they should be.

--- ScrollDer/Views/HomeView.swift
+++ ScrollDer/Views/HomeView.swift
@@ -1,13 +1,13 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  ScrollDer
 //
-//  Created by Dariya Gecher on 15.02.2023.
+//  Created by Dariya Gecher on 19.02.2023.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 8.0) {
             Spacer()
@@ -52,9 +52,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-        ContentView()
+        HomeView()
     }
 }
