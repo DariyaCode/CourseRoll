db0420e493b9d51e5871cf9cf2df8787a9e9c95f
Dariya Gecher <dariya.gecher@gmail.com>
2023-02-27 23:45:54 +0600

    Edited and added to the code the ability to exit the detailed course information screen by pulling from the left corner.

--- ScrollDer/Views/CourseView.swift
+++ ScrollDer/Views/CourseView.swift
@@ -13,6 +13,8 @@ struct CourseView: View {
     @Binding var show: Bool
     @State var appear = [false, false, false]
     @EnvironmentObject var model: Model
+    @State var viewState: CGSize = .zero
+    @State var isDraggable = true
     
     var body: some View {
         ZStack {
@@ -25,18 +27,24 @@ struct CourseView: View {
                     .opacity(appear[2] ? 1 : 0)
             }
             .background(Color("Background"))
+            .mask(RoundedRectangle(cornerRadius: viewState.width / 3, style: .continuous))
+            .shadow(color: .black.opacity(0.3), radius: 30, x: 0, y: 10)
+            .scaleEffect(viewState.width / -500 + 1)
+            .background(.black.opacity(viewState.width / 500))
+            .background(.ultraThinMaterial)
+            .gesture( isDraggable ? drag : nil )
             .ignoresSafeArea()
             
             button
             
-            }
+        }
         .onAppear{
             fadeIn()
         }
         .onChange(of: show){ newValue in
             fadeOut()
         }
-        }
+    }
     
     var cover: some View{
         GeometryReader { proxy in
@@ -73,7 +81,7 @@ struct CourseView: View {
             .overlay(
                 overlayContent
                     .offset(y: scrollY > 0 ? scrollY * -0.6 : 0)
-        )
+            )
         }
         .frame(height: 500)
     }
@@ -98,78 +106,117 @@ struct CourseView: View {
                 show.toggle()
                 model.showDetail.toggle()
             }
-            } label : {
-                Image(systemName: "xmark")
-                    .font(.body.weight(.bold))
-                    .foregroundColor(.secondary)
-                    .padding(8)
-                    .background(.ultraThinMaterial, in: Circle())
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
-            .padding(20)
-            .ignoresSafeArea()
+        } label : {
+            Image(systemName: "xmark")
+                .font(.body.weight(.bold))
+                .foregroundColor(.secondary)
+                .padding(8)
+                .background(.ultraThinMaterial, in: Circle())
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+        .padding(20)
+        .ignoresSafeArea()
     }
     
     var overlayContent: some View{
-            VStack(alignment: .leading, spacing: 12) {
-                Text(course.title)
-                    .matchedGeometryEffect(id: "title\(course.id)", in: namespace)
-                    .font(.largeTitle.weight(.bold))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Text(course.subtitle.uppercased())
-                    .font(.footnote.weight(.semibold))
-                    .matchedGeometryEffect(id: "subtitle\(course.id)", in: namespace)
-                Text(course.text)
-                    .font(.footnote)
-                    .matchedGeometryEffect(id: "text\(course.id)", in: namespace)
-                Divider()
-                    .opacity(appear[0] ? 1 : 0)
-                HStack{
-                    Image("Avatar Default")
-                        .resizable()
-                        .frame(width: 26, height: 26)
-                        .cornerRadius(10)
-                        .padding(8)
-                        .background(.ultraThinMaterial, in:
-                                        RoundedRectangle(
-                                            cornerRadius: 18, style: .continuous)
-                        )
-                        .strokeStyle(cornerRadius: 18)
-                    Text("Taught Dariya Gecher")
-                        .font(.footnote)
-                }
-                .opacity(appear[1] ? 1 : 0)
-            }
-            .padding(20)
-            .background(
-                Rectangle()
-                    .fill(.ultraThinMaterial)
-                    .mask(RoundedRectangle(
-                        cornerRadius: 30, style: .continuous)
+        VStack(alignment: .leading, spacing: 12) {
+            Text(course.title)
+                .matchedGeometryEffect(id: "title\(course.id)", in: namespace)
+                .font(.largeTitle.weight(.bold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text(course.subtitle.uppercased())
+                .font(.footnote.weight(.semibold))
+                .matchedGeometryEffect(id: "subtitle\(course.id)", in: namespace)
+            Text(course.text)
+                .font(.footnote)
+                .matchedGeometryEffect(id: "text\(course.id)", in: namespace)
+            Divider()
+                .opacity(appear[0] ? 1 : 0)
+            HStack{
+                Image("Avatar Default")
+                    .resizable()
+                    .frame(width: 26, height: 26)
+                    .cornerRadius(10)
+                    .padding(8)
+                    .background(.ultraThinMaterial, in:
+                                    RoundedRectangle(
+                                        cornerRadius: 18, style: .continuous)
                     )
-                    .matchedGeometryEffect(id: "blur\(course.id)", in: namespace)
-            )
-            .offset(y: 250)
-            .padding(20)
+                    .strokeStyle(cornerRadius: 18)
+                Text("Taught Dariya Gecher")
+                    .font(.footnote)
+            }
+            .opacity(appear[1] ? 1 : 0)
+        }
+        .padding(20)
+        .background(
+            Rectangle()
+                .fill(.ultraThinMaterial)
+                .mask(RoundedRectangle(
+                    cornerRadius: 30, style: .continuous)
+                )
+                .matchedGeometryEffect(id: "blur\(course.id)", in: namespace)
+        )
+        .offset(y: 250)
+        .padding(20)
+        
+    }
+    
+    var drag: some Gesture {
+        
+        DragGesture(minimumDistance: 30, coordinateSpace: .local).onEnded { value in
+            guard value.translation.width > 0 else { return }
             
+            if value.startLocation.x < 100{
+                withAnimation(.closeCard){
+                    viewState = value.translation
+                }
+            }
+            
+            if viewState.width > 120 {
+                close()
+            }
+        }
+        .onEnded{ value in
+            if viewState.width > 80 {
+                close()
+            } else {
+                withAnimation(.closeCard) {
+                    viewState = .zero
+                }
+            }
+        }
     }
     
     func fadeIn(){
-            withAnimation(.easeInOut(duration: 0.3)){
-                appear[0] = true
-            }
-            withAnimation(.easeInOut(duration: 0.4)){
-                appear[1] = true
-            }
-            withAnimation(.easeInOut(duration: 0.5)){
-                appear[2] = true
-            }
+        withAnimation(.easeInOut(duration: 0.3)){
+            appear[0] = true
+        }
+        withAnimation(.easeInOut(duration: 0.4)){
+            appear[1] = true
+        }
+        withAnimation(.easeInOut(duration: 0.5)){
+            appear[2] = true
+        }
     }
     
     func fadeOut(){
         appear[0] = false
         appear[1] = false
         appear[2] = false
+    }
+    
+    func close(){
+        withAnimation(.closeCard.delay(0.3)){
+            show.toggle()
+            model.showDetail.toggle()
+        }
+        
+        withAnimation(.closeCard){
+            viewState = .zero
+        }
+        
+        isDraggable = false
     }
 }
 
