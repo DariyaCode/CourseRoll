bc3f71f9e5df44d7344d51eaead49d937614ef7e
Dariya Gecher <dariya.gecher@gmail.com>
2023-03-27 15:35:02 +0600

    Added fields for entering email and password to the registration window.  These styles are in a separate file.

--- ScrollDer/Views/SignUpView.swift
+++ ScrollDer/Views/SignUpView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State var email = ""
+    @State var password = ""
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             Text("Sign up")
                 .font(.largeTitle).bold()
             Text("Access 120+ hours of courses, tutorials and livestreams")
                 .font(.headline)
+            TextField("Email", text: $email)
+                .inputStyle(icon: "mail")
+                .textContentType(.emailAddress)
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+            SecureField("Password", text: $password)
+                .inputStyle(icon: "lock")
+                .textContentType(.password)
             Button{} label: {
                 Text("Create an account")
                     .frame(maxWidth: .infinity)
@@ -23,6 +34,25 @@ struct SignUpView: View {
             .buttonStyle(.angular)
             .tint(.accentColor)
             .controlSize(.large)
+            
+            
+            Group {
+                Text("By clicking on ")
+                + Text("__Create an account__").foregroundColor(.primary.opacity(0.7))
+                + Text(", you agree to our **Terms of Service** and **[Privacy Policy](https://apple.com)**")
+                
+                Divider()
+                
+                HStack{
+                    Text("Already have an accont?")
+                    Button {} label: {
+                        Text("**Sign in**")
+                    }
+                }
+            }
+            .font(.footnote)
+            .foregroundColor(.secondary)
+            .accentColor(.secondary)
         }
         .padding(20)
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
